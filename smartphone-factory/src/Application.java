0372f7024627c204a3059995c6d20f266d9e5544
Yulia Bizunova <yulia320455@gmail.com>
2024-05-22 14:25:19 +0100

    Correct 2: method choiceOfOrder moved to Application correct  method makeOrder()

--- smartphone-factory/src/Application.java
+++ smartphone-factory/src/Application.java
@@ -1,9 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDateTime;
 
 public class Application {
     public final static BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void choiceOfOrder() throws IOException {
+
+        LocalDateTime dataTimeOfOrder = LocalDateTime.now();
+        int quatityOfSmartphones = 0;
+        String nameOfSmartphone = null;
+        String modelOfSmartphone;
+        int volumeMemory = 0;
+        boolean isRight = false;
+
+        //quantity of smartphones
+        do {
+            try {
+                System.out.println("Write the quantity of smartphones");
+                quatityOfSmartphones = Integer.parseInt(Application.READER.readLine());
+                isRight = true;
+            } catch (NumberFormatException e) {
+                System.out.println("You have written a wrong information. Try again.");
+            }
+        } while (!isRight);
+
+        //name of brand
+        System.out.println("Write the name of the brand (for example: APPLE, SAMSUNG, GOOGLE, NOKIA, HONOR, ALCATEL or REALME)");
+        nameOfSmartphone = Application.READER.readLine().toUpperCase();
+
+        //model
+        isRight = false;
+        System.out.println("Write the model");
+        modelOfSmartphone = Application.READER.readLine();
+
+        //volume of memory
+        do {
+            try {
+                System.out.println("Write the volume of memory (for example: 64, 128 or 256GB)");
+                volumeMemory = Integer.parseInt(Application.READER.readLine());
+                isRight = true;
+            } catch (NumberFormatException e) {
+                System.out.println("You have written a wrong information. Try again.");
+            }
+        } while (!isRight);
+
+        //size of screen
+        System.out.println("Write the screenSize (for example:480×800, 640×1136, 720×1280, 750×1334, 1080×1920 or 1440×2560)");
+        String screenSize = Application.READER.readLine();
+
+        //Add order to queue
+        SmartphoneFactory.queueOfOrders.offer(new Order(dataTimeOfOrder, Status.CREATED,
+                new Smartphone(nameOfSmartphone, modelOfSmartphone, volumeMemory, screenSize), quatityOfSmartphones));
+        System.out.println("Currently your order is in the queue, please stand by.");
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         SmartphoneFactory smartphoneFactory = new SmartphoneFactory(Runtime.getRuntime().availableProcessors());
@@ -14,13 +65,9 @@ public class Application {
                 System.out.println("1. Make your order. 0. Exit.");
                 choice = Integer.parseInt(READER.readLine());
                 if (choice == 1) {
-                    boolean isEmpty = SmartphoneFactory.queueOfOrders.isEmpty();
-                    smartphoneFactory.choiceOfOrder();
-                    if (isEmpty) {
-                        smartphoneFactory.produce();
-                        smartphoneFactory.join();
-                    }
-
+                    choiceOfOrder(); //get information,add to queue
+                    smartphoneFactory.makeOrder(); //check the queue, produce
+                    smartphoneFactory.join();
                 } else if (choice == 0) break;
             } while (choice != 0);
         }
--- smartphone-factory/src/SmartphoneFactory.java
+++ smartphone-factory/src/SmartphoneFactory.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -27,64 +26,17 @@ public class SmartphoneFactory extends Thread {
         this.availableProcessors = availableProcessors;
     }
 
-    public static void makeOrder(Order order) {
-        queueOfOrders.offer(order);
-    }
-
-    public static void choiceOfOrder() throws IOException {
-
-        LocalDateTime dataTimeOfOrder = LocalDateTime.now();
-        int quatityOfSmartphones = 0;
-        String nameOfSmartphone = null;
-        String modelOfSmartphone;
-        int volumeMemory = 0;
-        boolean isRight = false;
-
-        //quantity of smartphones
-        do {
-            try {
-                System.out.println("Write the quantity of smartphones");
-                quatityOfSmartphones = Integer.parseInt(Application.READER.readLine());
-                isRight = true;
-            } catch (NumberFormatException e) {
-                System.out.println("You have written a wrong information. Try again.");
-            }
-        } while (!isRight);
-
-        //name of brand
-        System.out.println("Write the name of the brand (for example: APPLE, SAMSUNG, GOOGLE, NOKIA, HONOR, ALCATEL or REALME)");
-        nameOfSmartphone = Application.READER.readLine().toUpperCase();
-
-        //model
-        isRight = false;
-        System.out.println("Write the model");
-        modelOfSmartphone = Application.READER.readLine();
-
-        //volume of memory
-        do {
-            try {
-                System.out.println("Write the volume of memory (for example: 64, 128 or 256GB)");
-                volumeMemory = Integer.parseInt(Application.READER.readLine());
-                isRight = true;
-            } catch (NumberFormatException e) {
-                System.out.println("You have written a wrong information. Try again.");
-            }
-        } while (!isRight);
-
-        //size of screen
-        System.out.println("Write the screenSize (for example:480×800, 640×1136, 720×1280, 750×1334, 1080×1920 or 1440×2560)");
-        String screenSize = Application.READER.readLine();
-
-        //Add order to queue
-        SmartphoneFactory.makeOrder(new Order(dataTimeOfOrder, Status.CREATED,
-                new Smartphone(nameOfSmartphone, modelOfSmartphone, volumeMemory, screenSize), quatityOfSmartphones));
+    public static void makeOrder() {
+        if (queueOfOrders.size() == 1) {
+            produce();
+        }
     }
 
     public static Smartphone produce() {
         queueOfOrders.peek().setStatusOfOrder(Status.CREATING);
         Smartphone producedSmartphone = queueOfOrders.peek().getSmartphone();
         int quatity = queueOfOrders.peek().getQuatityOfSmartphones();
-
+        System.out.println("Your order is producing.");
         while (quatity > 0) {
             try {
                 sleep(1000);
@@ -101,6 +53,5 @@ public class SmartphoneFactory extends Thread {
         queueOfOrders.poll();
         return producedSmartphone;
     }
-
 
 }
