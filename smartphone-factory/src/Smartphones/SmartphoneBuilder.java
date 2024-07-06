2cd2b4508ba7c1e627a066b4aa8ca9e2aa9ad370
Yulia Bizunova <yulia320455@gmail.com>
2024-07-06 01:48:20 +0100

    corrects of previous updates

--- smartphone-factory/src/Smartphones/SmartphoneBuilder.java
+++ smartphone-factory/src/Smartphones/SmartphoneBuilder.java
@@ -4,6 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public interface SmartphoneBuilder {
-    void buildSmartphone(BufferedReader READER) throws IOException;
+    void buildSmartphone(BufferedReader reader) throws IOException;
       Smartphone getSmartphone();
 }
